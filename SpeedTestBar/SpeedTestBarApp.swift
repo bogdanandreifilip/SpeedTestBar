bf0d418c2bf3796957314cbb108c6cedd4db7ebf
bogdanandreifilip <bogdanandrei.filip@gmail.com>
2026-04-27 22:34:18 +0300

    Initial Commit

---
+++ SpeedTestBar/SpeedTestBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeedTestBarApp.swift
+//  SpeedTestBar
+//
+//  Created by Filip Bogdan on 27/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct SpeedTestBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
